8eb86ff4fe88022d1d5f16f97b87eacb52fd291d
neomaxdigital <neomaxdigital@gmail.com>
2026-09-24 15:21:18 -0300

    Opt in to experimental window inset APIs

--- app/src/main/java/com/juke/mp3player/ui/components/JukeScreen.kt
+++ app/src/main/java/com/juke/mp3player/ui/components/JukeScreen.kt
@@ -3,6 +3,7 @@ package com.juke.mp3player.ui.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.juke.mp3player.ui.theme.JukeBlack
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun JukeScreen(content: @Composable BoxScope.() -> Unit) {
     val top = WindowInsets.statusBarsIgnoringVisibility.asPaddingValues().calculateTopPadding()
